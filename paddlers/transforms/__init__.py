39796e2de438d8d47b36e18e9be945fe47d65ba4
geoyee <geoyee@yeah.net>
2022-03-03 22:12:21 +0800

    [Fix] Update copyright

--- paddlers/transforms/__init__.py
+++ paddlers/transforms/__init__.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from operator import mod
 from .operators import *
 from .batch_operators import BatchRandomResize, BatchRandomResizeByShort, _BatchPadding
 from paddlers import transforms as T
