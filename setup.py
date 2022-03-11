17322ba21287ddf6467b0ba82aff708ec1875dd5
Yizhou Chen <geoyee@yeah.net>
2022-03-11 17:07:59 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,25 +14,33 @@
 
 import setuptools
 
-long_description = "Awesome Remote Sensing Toolkit based on PaddlePaddle"
+
+DESCRIPTION = "Awesome Remote Sensing Toolkit based on PaddlePaddle"
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+with open("requirements.txt") as fin:
+    REQUIRED_PACKAGES = fin.read()
 
 setuptools.setup(
     name="paddlers",
     version='0.0.1',
     author="paddlers",
     author_email="paddlers@baidu.com",
-    description=long_description,
-    long_description=long_description,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/plain",
     url="https://github.com/PaddleCV-SIG/PaddleRS",
     packages=setuptools.find_packages(),
     setup_requires=['cython', 'numpy'],
-    install_requires=[
-        "pycocotools", 'pyyaml', 'colorama', 'tqdm', 'paddleslim==2.2.1',
-        'visualdl>=2.2.2', 'shapely>=1.7.0', 'opencv-python', 'scipy', 'lap',
-        'motmetrics', 'scikit-learn==0.23.2', 'chardet', 'flask_cors',
-        'openpyxl', 'gdal'
-    ],
+    install_requires=REQUIRED_PACKAGES,
+    # [
+    #     "pycocotools", 'pyyaml', 'colorama', 'tqdm', 'paddleslim==2.2.1',
+    #     'visualdl>=2.2.2', 'shapely>=1.7.0', 'opencv-python', 'scipy', 'lap',
+    #     'motmetrics', 'scikit-learn==0.23.2', 'chardet', 'flask_cors',
+    #     'openpyxl', 'gdal'
+    # ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
