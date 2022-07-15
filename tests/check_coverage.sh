7a65f63c5d3542863825a6021198fd94131be00d
Bobholamovic <mhlin425@whu.edu.cn>
2022-07-15 20:35:21 +0800

    Add scripts for unittests

--- tests/check_coverage.sh
+++ tests/check_coverage.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
-coverage run -m unittest discover
+export PYTHONPATH=${PYTHONPATH}:'../tools'
+coverage run --source paddlers,$(ls -d ../tools/* | tr '\n' ',') --omit=../paddlers/models/* -m unittest discover -v
 coverage report
 coverage html -d coverage_html
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 
+export PYTHONPATH=${PYTHONPATH}:'../tools'
 python -m unittest discover -v
