dedb77d3ad55ea48eca9a56e9c6d6e433cfb68f6
Bobholamovic <bob1998425@hotmail.com>
2022-07-31 16:19:55 +0800

    Fix typo

--- tests/check_coverage.sh
+++ tests/check_coverage.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-bash download_test_data
+bash download_test_data.sh
 coverage run --source paddlers,$(ls -d ../tools/* | tr '\n' ',') --omit=../paddlers/models/* -m unittest discover -v
 coverage report
 coverage html -d coverage_html
--- tests/run_fast_tests.sh
+++ tests/run_fast_tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-bash download_test_data
+bash download_test_data.sh
 python -m unittest -v fast_tests
