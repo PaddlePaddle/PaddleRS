0b54a9071a51655b616531f7eb9f2be069ee1edb
Bobholamovic <mhlin425@whu.edu.cn>
2022-08-23 21:07:50 +0800

    Refactor tests

--- tests/run_ci_dev.sh
+++ tests/run_ci_dev.sh
@@ -39,14 +39,3 @@ set -e
 
 cd tests/
 bash run_fast_tests.sh
-
-cd ..
-for config in $(ls test_tipc/configs/*/*/train_infer_python.txt); do
-    bash test_tipc/prepare.sh ${config} lite_train_lite_infer
-    bash test_tipc/test_train_inference_python.sh ${config} lite_train_lite_infer
-    task="$(basename $(dirname $(dirname ${config})))"
-    model="$(basename $(dirname ${config}))"
-    if grep -q "test_tipc/output/${task}/${model}/lite_train_lite_infer/results_python.log"; then
-        exit 1
-    fi
-done
