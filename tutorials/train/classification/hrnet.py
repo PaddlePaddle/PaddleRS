9f5c87e9dd8bc2817b31388b1c1491c577ad7cf6
Bobholamovic <bob1998425@hotmail.com>
2022-09-12 14:20:36 +0800

    Rename clas models

--- tutorials/train/classification/hrnet.py
+++ tutorials/train/classification/hrnet.py
@@ -68,7 +68,7 @@ eval_dataset = pdrs.datasets.ClasDataset(
 # 构建HRNet模型
 # 目前已支持的模型请参考：https://github.com/PaddlePaddle/PaddleRS/blob/develop/docs/intro/model_zoo.md
 # 模型输入参数请参考：https://github.com/PaddlePaddle/PaddleRS/blob/develop/paddlers/tasks/classifier.py
-model = pdrs.tasks.clas.HRNet_W18_C(num_classes=len(train_dataset.labels))
+model = pdrs.tasks.clas.HRNet(num_classes=len(train_dataset.labels))
 
 # 执行模型训练
 model.train(
