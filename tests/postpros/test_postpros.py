9434046a13cd8a5f1d7f2d8a49ed94e51cf9e907
geoyee <geoyee@yeah.net>
2023-02-15 20:58:17 +0800

    [Test] Update postpro test

--- tests/postpros/test_postpros.py
+++ tests/postpros/test_postpros.py
@@ -35,89 +35,81 @@ class TestPostProgress(CpuCommonTest):
     def test_prepro_mask(self):
         mask = copy.deepcopy(self.b_label)
         mask = P.prepro_mask(mask)
-        self.assertEqual(len(mask.shape), 2)
+        self.check_output_equal(len(mask.shape), 2)
         self.assertEqual(mask.dtype, np.uint8)
-        self.assertEqual(tuple(np.unique(mask)), (0, 1))
+        self.check_output_equal(np.unique(mask), np.array([0, 1]))
         mask_tensor = paddle.randn((1, 3, 256, 256), dtype="float32")
         mask_tensor = P.prepro_mask(mask_tensor)
-        self.assertEqual(len(mask_tensor.shape), 2)
+        self.check_output_equal(len(mask_tensor.shape), 2)
         self.assertEqual(mask_tensor.dtype, np.uint8)
-        self.assertEqual(tuple(np.unique(mask_tensor)), (0, 1, 2))
+        self.check_output_equal(np.unique(mask_tensor), np.array([0, 1, 2]))
 
     def test_del_small_connection(self):
         mask = copy.deepcopy(self.b_label)
         mask = P.prepro_mask(mask)
         mask = P.del_small_connection(mask)
-        self.assertEqual(mask.shape, self.b_label.shape)
+        self.check_output_equal(mask.shape, self.b_label.shape)
         self.assertEqual(mask.dtype, self.b_label.dtype)
-        self.assertEqual(
-            np.unique(mask).tolist(), np.unique(self.b_label).tolist())
+        self.check_output_equal(np.unique(mask), np.unique(self.b_label))
 
     def test_fill_small_holes(self):
         mask = copy.deepcopy(self.b_label)
         mask = P.prepro_mask(mask)
         mask = P.fill_small_holes(mask)
-        self.assertEqual(mask.shape, self.b_label.shape)
+        self.check_output_equal(mask.shape, self.b_label.shape)
         self.assertEqual(mask.dtype, self.b_label.dtype)
-        self.assertEqual(
-            np.unique(mask).tolist(), np.unique(self.b_label).tolist())
+        self.check_output_equal(np.unique(mask), np.unique(self.b_label))
 
     def test_morphological_operation(self):
         mask = copy.deepcopy(self.b_label)
         mask = P.prepro_mask(mask)
         for op in ["open", "close", "erode", "dilate"]:
             mask = P.morphological_operation(mask, op)
-            self.assertEqual(mask.shape, self.b_label.shape)
+            self.check_output_equal(mask.shape, self.b_label.shape)
             self.assertEqual(mask.dtype, self.b_label.dtype)
-            self.assertEqual(
-                np.unique(mask).tolist(), np.unique(self.b_label).tolist())
+            self.check_output_equal(np.unique(mask), np.unique(self.b_label))
 
     def test_building_regularization(self):
         mask = copy.deepcopy(self.b_label)
         mask = P.prepro_mask(mask)
         mask = P.building_regularization(mask)
-        self.assertEqual(mask.shape, self.b_label.shape)
+        self.check_output_equal(mask.shape, self.b_label.shape)
         self.assertEqual(mask.dtype, self.b_label.dtype)
-        self.assertEqual(
-            np.unique(mask).tolist(), np.unique(self.b_label).tolist())
+        self.check_output_equal(np.unique(mask), np.unique(self.b_label))
 
     def test_cut_road_connection(self):
         mask = copy.deepcopy(self.b_label)
         mask = P.prepro_mask(mask)
         mask = P.cut_road_connection(mask)
-        self.assertEqual(mask.shape, self.b_label.shape)
+        self.check_output_equal(mask.shape, self.b_label.shape)
         self.assertEqual(mask.dtype, self.b_label.dtype)
-        self.assertEqual(
-            np.unique(mask).tolist(), np.unique(self.b_label).tolist())
+        self.check_output_equal(np.unique(mask), np.unique(self.b_label))
 
     def test_conditional_random_field(self):
         if "conditional_random_field" in dir(P):
             mask = copy.deepcopy(self.m_label)
             mask = P.prepro_mask(mask)
             mask = P.conditional_random_field(self.image2, mask)
-            self.assertEqual(mask.shape, self.m_label.shape)
+            self.check_output_equal(mask.shape, self.m_label.shape)
             self.assertEqual(mask.dtype, self.m_label.dtype)
-            self.assertEqual(
-                np.unique(mask).tolist(), np.unique(self.m_label).tolist())
+            self.check_output_equal(np.unique(mask), np.unique(self.m_label))
 
     def test_markov_random_field(self):
         mask = copy.deepcopy(self.m_label)
         mask = P.prepro_mask(mask)
         mask = P.markov_random_field(self.image2, mask)
-        self.assertEqual(mask.shape, self.m_label.shape)
+        self.check_output_equal(mask.shape, self.m_label.shape)
         self.assertEqual(mask.dtype, self.m_label.dtype)
-        self.assertEqual(
-            np.unique(mask).tolist(), np.unique(self.m_label).tolist())
+        self.check_output_equal(np.unique(mask), np.unique(self.m_label))
 
     def test_deal_one_class(self):
         mask = copy.deepcopy(self.m_label)
         mask = P.prepro_mask(mask)
         func = P.morphological_operation
         mask = P.deal_one_class(mask, 1, func, ops="dilate")
-        self.assertEqual(mask.shape, self.m_label.shape)
+        self.check_output_equal(mask.shape, self.m_label.shape)
         self.assertEqual(mask.dtype, self.m_label.dtype)
-        self.assertEqual(
-            np.unique(mask).tolist(), np.unique(self.m_label).tolist())
+        self.check_output_equal(np.unique(mask), np.unique(self.m_label))
 
     def test_change_(self):
         mask = copy.deepcopy(self.m_label)
@@ -126,7 +118,6 @@ class TestPostProgress(CpuCommonTest):
                                          0.8, "GLI", {"b": 3,
                                                       "g": 2,
                                                       "r": 1})
-        self.assertEqual(mask.shape, self.m_label.shape)
+        self.check_output_equal(mask.shape, self.m_label.shape)
         self.assertEqual(mask.dtype, self.m_label.dtype)
-        self.assertEqual(
-            np.unique(mask).tolist(), np.unique(self.m_label).tolist())
+        self.check_output_equal(np.unique(mask), np.unique(self.m_label))
