920dc44c810b52be0fdd13501f73dddbc560b759
geoyee <geoyee@yeah.net>
2023-03-03 11:55:41 +0800

    [Docs] Update tutorials

--- tutorials/train/classification/hrnet.py
+++ tutorials/train/classification/hrnet.py
@@ -24,9 +24,7 @@ pdrs.utils.download_and_decompress(
 # 定义训练和验证时使用的数据变换（数据增强、预处理等）
 # 使用Compose组合多种变换方式。Compose中包含的变换将按顺序串行执行
 # API说明：https://github.com/PaddlePaddle/PaddleRS/blob/develop/docs/apis/data.md
-train_transforms = T.Compose([
-    # 读取影像
-    T.DecodeImg(),
+train_transforms = [
     # 将影像缩放到256x256大小
     T.Resize(target_size=256),
     # 以50%的概率实施随机水平翻转
@@ -35,18 +33,15 @@ train_transforms = T.Compose([
     T.RandomVerticalFlip(prob=0.5),
     # 将数据归一化到[-1,1]
     T.Normalize(
-        mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-    T.ArrangeClassifier('train')
-])
+        mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+]
 
-eval_transforms = T.Compose([
-    T.DecodeImg(),
+eval_transforms = [
     T.Resize(target_size=256),
     # 验证阶段与训练阶段的数据归一化方式必须相同
     T.Normalize(
-        mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-    T.ArrangeClassifier('eval')
-])
+        mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+]
 
 # 分别构建训练和验证所用的数据集
 train_dataset = pdrs.datasets.ClasDataset(
