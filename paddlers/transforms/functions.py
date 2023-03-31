a986d048f171ef5f154384dbbc73b550092a1dbd
Yizhou Chen <geoyee@yeah.net>
2023-03-31 14:57:01 +0800

    Update functions.py

--- paddlers/transforms/functions.py
+++ paddlers/transforms/functions.py
@@ -430,7 +430,7 @@ def to_uint8(im, norm=True, stretch=False):
         return stretched_img
 
     dtype = im.dtype.name
-    if dtype[:-2] == "uint" and np.max(dtype) < 256:
+    if dtype[:-2] == "uint" and np.max(im) < 256:
         pass
     else:
         if norm:
