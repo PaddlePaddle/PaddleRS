a13ad894b82362bcdad3d27ba3023cb30cf5291c
geoyee <geoyee@yeah.net>
2023-03-31 13:01:13 +0800

    [Fix] Fix to_uint8

--- paddlers/transforms/functions.py
+++ paddlers/transforms/functions.py
@@ -430,12 +430,15 @@ def to_uint8(im, norm=True, stretch=False):
         return stretched_img
 
     dtype = im.dtype.name
-    if dtype != "uint8" and norm:
-        im = _sample_norm(im)
-    if stretch:
-        im = _two_percent_linear(im)
-    im = np.uint8(im * 255)
-    return im
+    if dtype[:-2] == "uint" and np.max(dtype) < 256:
+        return im
+    else:
+        if norm:
+            im = _sample_norm(im)
+        if stretch:
+            im = _two_percent_linear(im)
+        im *= 255
+    return np.uint8(im)
 
 
 def to_intensity(im):
